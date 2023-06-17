9745651a953c50e756ee0fa63b0261f9177a93f5
Kevin A <hello@cinderblockgames.com>
2023-06-17 17:10:25 -0400

    Updating version string.

--- NanoPublicApi/Program.cs
+++ NanoPublicApi/Program.cs
@@ -4,7 +4,7 @@ using NanoPublicApi.Config;
 using NanoPublicApi.Connectors;
 
 const string COIN = "NANO";
-const string VERSION = "V25.0";
+const string VERSION = "V25.1";
 
 var env = GetEnvironmentVariables();
 var node = env["NODE"];
